b4c19006d72368dcad290b5faae0f76076aced00
František Milt <frantisek.milt@gmail.com>
2016-01-13 16:40:28 +0100

    Implemented TEvent, TMutex and TSemaphore classes, changes in implementation of TWinSyncObject class

--- WinSyncObjs.pas
+++ WinSyncObjs.pas
@@ -3,9 +3,15 @@ unit WinSyncObjs;
 interface
 
 uses
-  Windows; 
+  Windows;
+
+const
+  SEMAPHORE_MODIFY_STATE = $00000002;
+  SEMAPHORE_ALL_ACCESS   = STANDARD_RIGHTS_REQUIRED or SYNCHRONIZE or $3;
 
 type
+  TWaitResult = (wrSignaled, wrTimeout, wrAbandoned, wrError);
+
   TCriticalSection = class(TObject)
   private
     fCriticalSectionObj:  TRTLCriticalSection;
@@ -27,14 +33,63 @@ type
     fHandle:      THandle;
     fLastError:   Integer;
     fName:        String;
+  protected
+    Function SetAndRectifyName(const Name: String): Boolean; virtual;
+    procedure SetAndCheckHandle(Handle: THandle); virtual;
   public
     destructor Destroy; override;
+    Function WaitFor(Timeout: DWORD = INFINITE): TWaitResult; virtual;
     property Handle: THandle read fHandle;
     property LastError: Integer read fLastError;
     property Name: String read fName;
   end;
 
+  TEvent = class(TWinSyncObject)
+  public
+    constructor Create(SecurityAttributes: PSecurityAttributes; ManualReset, InitialState: Boolean; const Name: String); overload;
+    constructor Create(const Name: String); overload;
+    constructor Create; overload;
+    constructor Open(DesiredAccess: DWORD; InheritHandle: Boolean; const Name: String); overload;
+    constructor Open(const Name: String); overload;
+    Function WaitForAndReset(Timeout: DWORD = INFINITE): TWaitResult;
+    Function SetEvent: Boolean;
+    Function ResetEvent: Boolean;
+  {
+    Function PulseEvent is unreliable and should not be used. More info here:
+    https://msdn.microsoft.com/en-us/library/windows/desktop/ms684914%28v=vs.85%29.aspx
+  }
+    Function PulseEvent: Boolean; deprecated;
+  end;
+
+  TMutex = class(TWinSyncObject)
+  public
+    constructor Create(SecurityAttributes: PSecurityAttributes; InitialOwner: Boolean; const Name: String); overload;
+    constructor Create(const Name: String); overload;
+    constructor Create; overload;
+    constructor Open(DesiredAccess: DWORD; InheritHandle: Boolean; const Name: String); overload;
+    constructor Open(const Name: String); overload;
+    Function WaitForAndRelease(TimeOut: DWORD = INFINITE): TWaitResult;
+    Function ReleaseMutex: Boolean;
+  end;
+
+  TSemaphore = class(TWinSyncObject)
+  public
+    constructor Create(SecurityAttributes: PSecurityAttributes; InitialCount, MaximumCount: Integer; const Name: String); overload;
+    constructor Create(InitialCount, MaximumCount: Integer; const Name: String); overload;
+    constructor Create(InitialCount, MaximumCount: Integer); overload;
+    constructor Open(DesiredAccess: LongWord; InheritHandle: Boolean; const Name: String); overload;
+    constructor Open(const Name: String); overload;
+    Function WaitForAndRelease(TimeOut: LongWord = INFINITE): TWaitResult;
+    Function ReleaseSemaphore(ReleaseCount: Integer; out PreviousCount: Integer): Boolean; overload;
+    Function ReleaseSemaphore: Boolean; overload;
+  end;
+
+  //TWaitableTimer = class(TWinSyncObject); ??  
+
 implementation
+
+uses
+  SysUtils;
 
 procedure TCriticalSection._SetSpinCount(Value: DWORD);
 begin
@@ -99,10 +154,252 @@ end;
 
 //==============================================================================
 
+Function TWinSyncObject.SetAndRectifyName(const Name: String): Boolean;
+begin
+fName := Name;
+If Length(fName) > MAX_PATH then SetLength(fName,MAX_PATH);
+Result := Length(fName) > 0;
+end;
+
+//------------------------------------------------------------------------------
+
+procedure TWinSyncObject.SetAndCheckHandle(Handle: THandle);
+begin
+fHandle := Handle;
+If fHandle = 0 then
+  begin
+    fLastError := GetLastError;
+    RaiseLastOSError;
+  end;
+end;
+
+//------------------------------------------------------------------------------
+
 destructor TWinSyncObject.Destroy;
 begin
 CloseHandle(fHandle);
 inherited;
+end;
+
+//------------------------------------------------------------------------------
+
+Function TWinSyncObject.WaitFor(Timeout: DWORD = INFINITE): TWaitResult;
+begin
+case WaitForSingleObject(fHandle,Timeout) of
+  WAIT_ABANDONED: Result := wrAbandoned;
+  WAIT_OBJECT_0:  Result := wrSignaled;
+  WAIT_TIMEOUT:   Result := wrTimeout;
+  WAIT_FAILED:    begin
+                    Result := wrError;
+                    FLastError := GetLastError;
+                  end;
+else
+  Result := wrError;
+  FLastError := GetLastError;
+end;
+end;
+
+//==============================================================================
+
+constructor TEvent.Create(SecurityAttributes: PSecurityAttributes; ManualReset, InitialState: Boolean; const Name: String);
+begin
+inherited Create;
+If SetAndRectifyName(Name) then
+  SetAndCheckHandle(CreateEvent(SecurityAttributes,ManualReset,InitialState,PChar(fName)))
+else
+  SetAndCheckHandle(CreateEvent(SecurityAttributes,ManualReset,InitialState,nil));
+end;
+
+//   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---
+
+constructor TEvent.Create(const Name: String);
+begin
+Create(nil,True,False,Name);
+end;
+
+//   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---
+
+constructor TEvent.Create;
+begin
+Create(nil,True,False,'');
+end;
+
+//------------------------------------------------------------------------------
+
+constructor TEvent.Open(DesiredAccess: DWORD; InheritHandle: Boolean; const Name: String);
+begin
+inherited Create;
+SetAndRectifyName(Name);
+SetAndCheckHandle(OpenEvent(DesiredAccess,InheritHandle,PChar(fName)));
+end;
+
+//   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---
+
+constructor TEvent.Open(const Name: String);
+begin
+Open(SYNCHRONIZE or EVENT_MODIFY_STATE,False,Name);
+end;
+
+//------------------------------------------------------------------------------
+
+Function TEvent.WaitForAndReset(Timeout: DWORD = INFINITE): TWaitResult;
+begin
+Result := WaitFor(Timeout);
+If Result = wrSignaled then ResetEvent;
+end;
+
+//------------------------------------------------------------------------------
+
+Function TEvent.SetEvent: Boolean;
+begin
+Result := Windows.SetEvent(fHandle);
+If not Result then
+  fLastError := GetLastError;
+end;
+
+//------------------------------------------------------------------------------
+
+Function TEvent.ResetEvent: Boolean;
+begin
+Result := Windows.ResetEvent(fHandle);
+If not Result then
+  fLastError := GetLastError;
+end;
+
+//------------------------------------------------------------------------------
+
+{$WARN SYMBOL_DEPRECATED OFF}
+Function TEvent.PulseEvent: Boolean;
+{$WARN SYMBOL_DEPRECATED ON}
+begin
+Result := Windows.PulseEvent(fHandle);
+If not Result then
+  fLastError := GetLastError;
+end;
+
+//==============================================================================
+
+constructor TMutex.Create(SecurityAttributes: PSecurityAttributes; InitialOwner: Boolean; const Name: String);
+begin
+inherited Create;
+If SetAndRectifyName(Name) then
+  SetAndCheckHandle(CreateMutex(SecurityAttributes,InitialOwner,PChar(fName)))
+else
+  SetAndCheckHandle(CreateMutex(SecurityAttributes,InitialOwner,nil));
+end;
+
+//   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---
+
+constructor TMutex.Create(const Name: String);
+begin
+Create(nil,False,Name);
+end;
+
+//   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---
+
+constructor TMutex.Create;
+begin
+Create(nil,False,'');
+end;
+
+//------------------------------------------------------------------------------
+
+constructor TMutex.Open(DesiredAccess: DWORD; InheritHandle: Boolean; const Name: String);
+begin
+inherited Create;
+SetAndRectifyName(Name);
+SetAndCheckHandle(OpenMutex(DesiredAccess,InheritHandle,PChar(fName)));
+end;
+
+//   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---
+
+constructor TMutex.Open(const Name: String);
+begin
+Open(SYNCHRONIZE or MUTEX_MODIFY_STATE,False,Name);
+end;
+
+//------------------------------------------------------------------------------
+
+Function TMutex.WaitForAndRelease(TimeOut: DWORD = INFINITE): TWaitResult;
+begin
+Result := WaitFor(Timeout);
+If Result in [wrSignaled,wrAbandoned] then ReleaseMutex;
+end;
+
+//------------------------------------------------------------------------------
+
+Function TMutex.ReleaseMutex: Boolean;
+begin
+Result := Windows.ReleaseMutex(fHandle);
+If not Result then
+  fLastError := GetLastError;
+end;
+
+//==============================================================================
+
+constructor TSemaphore.Create(SecurityAttributes: PSecurityAttributes; InitialCount, MaximumCount: Integer; const Name: String);
+begin
+inherited Create;
+If SetAndRectifyName(Name) then
+  SetAndCheckHandle(CreateSemaphore(SecurityAttributes,InitialCount,MaximumCount,PChar(fName)))
+else
+  SetAndCheckHandle(CreateSemaphore(SecurityAttributes,InitialCount,MaximumCount,nil));
+end;
+
+//   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---
+
+constructor TSemaphore.Create(InitialCount, MaximumCount: Integer; const Name: String);
+begin
+Create(nil,InitialCount,MaximumCount,Name);
+end;
+
+//   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---
+
+constructor TSemaphore.Create(InitialCount, MaximumCount: Integer);
+begin
+Create(nil,InitialCount,MaximumCount,'');
+end;
+
+//------------------------------------------------------------------------------
+
+constructor TSemaphore.Open(DesiredAccess: LongWord; InheritHandle: Boolean; const Name: String);
+begin
+inherited Create;
+SetAndRectifyName(Name);
+SetAndCheckHandle(OpenSemaphore(DesiredAccess,InheritHandle,PChar(fName)));
+end;
+ 
+//   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---
+
+constructor TSemaphore.Open(const Name: String);
+begin
+Open(SYNCHRONIZE or SEMAPHORE_MODIFY_STATE,False,Name);
+end;
+ 
+//------------------------------------------------------------------------------
+
+Function TSemaphore.WaitForAndRelease(TimeOut: LongWord = INFINITE): TWaitResult;
+begin
+Result := WaitFor(Timeout);
+If Result in [wrSignaled,wrAbandoned] then ReleaseSemaphore;
+end;
+
+//------------------------------------------------------------------------------
+
+Function TSemaphore.ReleaseSemaphore(ReleaseCount: Integer; out PreviousCount: Integer): Boolean;
+begin
+Result := Windows.ReleaseSemaphore(fHandle,ReleaseCount,@PreviousCount);
+If not Result then
+  fLastError := GetLastError;
+end;
+
+//   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---
+
+Function TSemaphore.ReleaseSemaphore: Boolean;
+var
+  Dummy:  Integer;
+begin
+Result := ReleaseSemaphore(1,Dummy);
 end;
 
 end.
